97cb97c5ac18221f05036c54d6703401ad2134a0
Greg Albers <geealbers@gmail.com>
2018-10-21 16:18:20 -0700

    Add script to run update of GitHub data, and add update date to banner

---
+++ bin/update.sh
@@ -0,0 +1,133 @@
+#!/bin/sh
+# Script to grab key data on GitHub organizations, their repos,
+# and their contributors and export it to a JSON file.
+# from --> https://github.com/geealbers/github-data
+
+
+# Add your GitHub 40-charcter authentication token:
+
+TOKEN=c569a70d354d0a6cf7f750684ef7f749da3c267e
+
+
+# Grab all museum user names from the data/museums.yml file
+
+USERS=($(sed -n 's/.*-[ ]\(.*\).*/\1/p' data/museums.yml))
+
+
+# Make a temporary directory to stash script working files
+# will be automatically deleted at the end
+
+mkdir bin/temp
+
+
+# Loop through list of users, make call to GitHub API
+# construct new JSON object with the needed information,
+# and save to tempoarary file
+
+# NEED FIX: Currently only gets the first page of results
+# with 100 repositories so the data on any org with more
+# repositories than that is incomplete
+
+for i in "${USERS[@]}"
+do
+
+  curl -H 'Authorization: token '$TOKEN'' "https://api.github.com/users/$i/repos?per_page=100" | jq '.[] | {
+    name_full: .full_name,
+    name: .name,
+    url: .html_url,
+    description: .description,
+    type: (if .fork == true then "Fork" else "Original" end),
+    date_created: (.created_at | fromdateiso8601 | strftime("%Y-%m-%d") ),
+    date_pushed: (.pushed_at | fromdateiso8601 | strftime("%Y-%m-%d") ),
+    size_kb: .size,
+    size_span: (
+        if (.size/1000) < 1 then "0-1 MB"
+      elif (.size/1000) < 10 then "1-10 MB"
+      elif (.size/1000) < 100 then "10-100 MB"
+      elif (.size/1000) < 1000 then "100-1000 MB"
+      else "1000+ MB" end),
+    stars: .stargazers_count,
+    language: .language,
+    forks: .forks_count,
+    license: (if .license.name then .license.name else .license end),
+    owner_name: .owner.login,
+    owner_url: .owner.html_url }' >> bin/temp/REPOS.txt
+
+done
+
+
+# The sections below follow the same pattern:
+#
+# 1. Create a temporary file with variable array
+# of repository names generated in the step above
+#
+# 2. Loop through that array, make call to GitHub API
+# for information, and create new array of objects
+
+
+# Get contributor names and contribution counts
+# for all museum repositories
+
+echo "REPO_NAMES=(" >> bin/temp/REPO_NAMES.txt
+
+cat bin/temp/REPOS.txt | jq --slurp ".[] | if .name_full == null then null else .name_full end" >> bin/temp/REPO_NAMES.txt
+
+echo ")" >> bin/temp/REPO_NAMES.txt
+
+source bin/temp/REPO_NAMES.txt
+
+for i in "${REPO_NAMES[@]}"
+do
+
+  curl -H 'Authorization: token '$TOKEN'' "https://api.github.com/repos/$i/contributors" | jq --arg repo "$i" '{
+    name_full: $repo,
+    contributor_count: (length),
+    contributor: [.[] | {name: .login, contributions: .contributions}]}' >> bin/temp/NAMES.txt
+
+done
+
+
+# Get the parent information for any museum
+# repository identified as a Fork
+
+echo "REPO_FORKS=(" >> bin/temp/REPO_FORKS.txt
+
+cat bin/temp/REPOS.txt | jq --slurp ".[] | if .type == \"Fork\" then .name_full else empty end" >> bin/temp/REPO_FORKS.txt
+
+echo ")" >> bin/temp/REPO_FORKS.txt
+
+source bin/temp/REPO_FORKS.txt
+
+for i in "${REPO_FORKS[@]}"
+do
+
+  curl -H 'Authorization: token '$TOKEN'' "https://api.github.com/repos/$i" | jq --arg repo "$i" '{
+    name_full: $repo,
+    parent: .parent.full_name,
+    parent_name: .parent.owner.login }' >> bin/temp/FORKS.txt
+
+done
+
+
+# Combine two resulting json texts from above (REPOS and PEOPLE),
+# group into arrays by repository name, and output a JSON file
+# with a timestamp.
+
+NOW=$(date +%F)
+
+jq --slurp --sort-keys '[group_by(.name_full) | .[] | add ]' bin/temp/NAMES.txt bin/temp/REPOS.txt bin/temp/FORKS.txt | tee data/github.json data/archive/github-$NOW.json
+
+sed -i -e 's/dataUpdated\:.*/dataUpdated: '$NOW'/g' config.yml
+rm -r config.yml-e
+# NEED FIX: I can’t get the sed command above to work like it’s supposed to
+# without creating a duplicate config.yml-e file
+# https://unix.stackexchange.com/questions/159367/using-sed-to-find-and-replace
+
+echo "There are ${#USERS[@]} museums with GitHub accounts."
+
+echo "A JSON file with all their public repository’s information has been created, and a dated copy has been added to the data/archive file."
+
+
+# Remove all temporary files
+
+rm -r bin/temp
